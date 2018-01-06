047401dc378e66cb53bb1b1cb53f4c2365058f9d
Arnav Gupta <arnav@codingblocks.com>
2018-01-06 18:23:25 +0530

    todolist complete
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture09/TodoWithBackend/public/script.js
+++ Lecture09/TodoWithBackend/public/script.js
@@ -1,25 +1,66 @@
 
 
-$.get('/showtodos', function (data) {
-    console.log(data)
-})
-
 $(function () {
 
     let task = $('#task')
+    let todolist = $('#todolist')
 
-    $('#add').click(function () {
 
-        $('#todolist').append(
-            $(`
+    function refreshTodos (todos) {
+        todolist.empty()
+        for (let i = 0; i < todos.length; i++) {
+            let todo = todos[i] // {task: jasgvjahv, done: }
+            if (todo.done) {
+                todo.done = 'checked'
+            } else {
+                todo.done = ''
+            }
+            todolist.append(
+                $(`
                 <div class="row my-4">
                     <div class="col"></div>
-                    <div class="col-6 todo-item">${task.val()}</div>
+                    <div class="col-1"><input onchange="dotodo(${i})" type="checkbox" ${todo.done}></div>
+                    <div class="col-6 todo-item">${todo.task}</div>
+                    <div class="col-1"></div>
                     <div class="col"></div>
                 </div>
             `)
-        )
+            )
+        }
+    }
+
+    window.dotodo = function (todoId) {
+        $.get(`/dotodo?id=${todoId}`, function (data) {
+            refreshTodos(data)
+        })
+    }
+
+    window.deletetodo = function(todoId) {
+        $.get(`/deletetodo?id=${todoId}`, function (data) {
+            refreshTodos(data)
+        })
+    }
+
+    $.get('/showtodos', function (data) {
+        refreshTodos(data)
+    })
+
+
+    $('#add').click(function () {
+
+
     })
 
 
 })
+
+
+// arr = [a,b,c,d,e,f,g]
+
+function moveUp(arr, i) {
+    arr.splice(i,0, arr.splice(i-1,1)[0])
+}
+
+function moveDown (arr, i) {
+    arr.splice(i,0, arr.splice(i+1,1)[0])
+}
